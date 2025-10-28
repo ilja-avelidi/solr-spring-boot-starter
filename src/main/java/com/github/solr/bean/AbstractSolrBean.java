a544de548f5bef04c2104cb41957aecd665bcf8d
ilja-avelidi <240711404+ilja-avelidi@users.noreply.github.com>
2025-10-28 18:24:26 +0100

    added abstract solr bean added dao layer for solr beans

---
+++ src/main/java/com/github/solr/bean/AbstractSolrBean.java
@@ -0,0 +1,16 @@
+package com.github.solr.bean;
+
+/**
+ * @author Ilja Avelidi
+ *
+ */
+public abstract class AbstractSolrBean {
+	
+	/**
+	 * Returns the unique identifier of this Solr Bean.
+	 * 
+	 * @return the unique id
+	 */
+	public abstract String getId();
+	
+}
