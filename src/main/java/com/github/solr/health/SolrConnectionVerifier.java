53311f875d6548b7371bc28762d045096978bfbd
ilja-avelidi <240711404+ilja-avelidi@users.noreply.github.com>
2025-10-28 18:43:39 +0100

    Added initial connection check Added an initial connection checker to verify Solr server is available.

---
+++ src/main/java/com/github/solr/health/SolrConnectionVerifier.java
@@ -0,0 +1,55 @@
+package com.github.solr.health;
+
+import org.apache.solr.client.solrj.SolrClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Ilja Avelidi
+ *
+ */
+@Component
+public class SolrConnectionVerifier implements ApplicationRunner {
+	
+	/**  */
+	private final SolrClient solrClient;
+	
+	/**  */
+	private final Logger log = LoggerFactory.getLogger(SolrConnectionVerifier.class);
+	
+	/**
+	 * @param solrClient
+	 */
+	public SolrConnectionVerifier(SolrClient solrClient) {
+		
+		this.solrClient = solrClient;
+		
+	}
+	
+	
+	/**
+	 * @see org.springframework.boot.ApplicationRunner#run(org.springframework.boot.ApplicationArguments)
+	 */
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		
+		try {
+			
+			this.log.info("Starting Solr connection check...");
+			
+			this.solrClient.ping();
+			
+			this.log.info("Successfully connected to Solr");
+			
+		} catch (Exception e) {
+			
+			this.log.error("Failed to connect to Solr: {}", e.getMessage(), e);
+			
+		}
+		
+	}
+	
+}
